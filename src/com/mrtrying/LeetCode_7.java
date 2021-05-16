92af985882fba819dda4026460a0228d84de9104
tanzeyu <tanzeyu@xiangha.com>
2021-05-16 17:07:48 +0800

    add:整数反转

---
+++ src/com/mrtrying/LeetCode_7.java
@@ -0,0 +1,30 @@
+package com.mrtrying;
+
+public class LeetCode_7 {
+
+    public static void main(String[] args) {
+        int num = 1534236469;
+        int reverse = new Solution().reverse(num);
+        System.out.println(reverse);
+    }
+
+    static class Solution {
+        public int reverse(int x) {
+            if(x < 10 && x > -10){
+                return x;
+            }
+            int symbol = x >= 0 ? 1 : -1;
+            x = Math.abs(x);
+            int length = String.valueOf(x).length();
+            int sum = 0;
+            for (int i = 0; i < length; i++) {
+                sum += (x % 10) * Math.pow(10, length - i - 1);
+                if(sum >= Integer.MAX_VALUE || sum <= Integer.MIN_VALUE){
+                    return 0;
+                }
+                x /= 10;
+            }
+            return sum * symbol;
+        }
+    }
+}
