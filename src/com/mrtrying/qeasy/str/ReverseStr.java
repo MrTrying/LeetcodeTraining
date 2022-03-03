328a2873411fab39b0f700b8df026e1727734ad7
zeyu_tan <306651457@qq.com>
2022-03-03 18:16:12 +0800

    fix:反转字符串（异或交换）

--- src/com/mrtrying/qeasy/str/ReverseStr.java
+++ src/com/mrtrying/qeasy/str/ReverseStr.java
@@ -5,8 +5,8 @@ import jdk.jshell.execution.Util;
 
 public class ReverseStr {
     public static void main(String[] args) {
-//        char[] s = {'h', 'e', 'l', 'l', 'o'};
-        char[] s = {'H','a','n','n','a','h'};
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+//        char[] s = {'H','a','n','n','a','h'};
         Solution solution = new Solution();
         solution.reverseString(s);
     }
@@ -22,10 +22,14 @@ public class ReverseStr {
 
     static class Solution {
         public void reverseString(char[] s) {
-            for (int i = 0; i < s.length / 2; i++) {
-                char temp = s[i];
-                s[i] = s[(s.length - 1) - i];
-                s[(s.length - 1) - i] = temp;
+            int length = s.length;
+            for (int i = 0; i < length / 2; i++) {
+//                char temp = s[i];
+//                s[i] = s[(length - 1) - i];
+//                s[(length - 1) - i] = temp;
+                s[i] ^= s[(length - 1) - i];
+                s[(length - 1) - i] ^=s[i];
+                s[i] ^= s[(length - 1) - i];
             }
             Utils.printArr(s);
         }
